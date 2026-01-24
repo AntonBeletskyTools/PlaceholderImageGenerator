8be98f365188325b3b913a2ab41d5f29ec14d7f2
Anton Beletsky <AntonBeletskyEU@gmail.com>
2026-01-24 20:30:36 +0200

    1

---
+++ how_to_use.bat
@@ -0,0 +1,3 @@
+asguard_placeholder.exe 800x600
+asguard_placeholder.exe 1920x1080 --bg #2c3e50 --text #ecf0f1
+asguard_placeholder.exe 100x100 --out logo_placeholder.png
---
+++ make_exe/make_exe.bat
@@ -0,0 +1,1 @@
+python -m PyInstaller --onefile --name "asguard_placeholder" placeholder.py
